d2ad3edc2ac0876e94cd2f8e31696028fc1bd44b
FiRe_MonQ <Fire.monq@gmail.com>
2018-11-23 22:51:22 +0300

    Lesson1 and Lesson2
    Add 2 lessons from CSharpFS

---
+++ CSharpFSLesson2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFSLesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose move for evening!");
+            Console.Write("Say your name:");
+            string username = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine(username + " what movie ganre you prefer?");
+            Console.WriteLine("- Comedy");
+            Console.WriteLine("- Drama");
+            Console.WriteLine("- Triller");
+            Console.WriteLine("- Western");
+            Console.WriteLine();
+
+            Console.Write("Type: ");
+            string ganre = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
