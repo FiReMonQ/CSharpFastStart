a0d6251b437e6d8c17c7fa8f10cd37b546e30c2f
FiRe_MonQ <fire.monq@fmail.com>
2018-11-24 17:01:29 +0300

    Lesson 5 complete

--- CSharpFSLesson5/Program.cs
+++ CSharpFSLesson5/Program.cs
@@ -9,66 +9,128 @@ namespace CSharpFSLesson5
 {
     class Program
     {
-
-        void Tletter()
+        static void Tword(int Tsize1, int Tsize2)
         {
             Turtle.Angle = 0;
-            Turtle.Move(50);
+            Turtle.Move(Tsize1);
 
             Turtle.TurnLeft();
-            Turtle.Move(25);
+            Turtle.Move(Tsize2);
 
             Turtle.Angle = 90;
-            Turtle.Move(50);
+            Turtle.Move(Tsize1);
 
         }
-
-        static void Main(string[] args)
+        static void Rword(int Rsize1, int Rsize2, int Rsize3)
         {
-                      
-            Turtle.Speed = 9;
-            Turtle.X = 100;
-            Turtle.Y = 200;
-            //Turtle.Hide();
-
-            //TORT
-
-            //"T" letter
-            Tletter();
-
-            //"O" letter
-            Turtle.X = 150;
-            Turtle.Y = 150;
-
-            Turtle.Angle = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                Turtle.TurnRight();
-                Turtle.Move( 50 );
-            }
-
-            //"R" letter
-            Turtle.X = 225;
-            Turtle.Y = 200;
-            
-            Turtle.Angle = 0;
-            Turtle.Move(50);
+            Turtle.Move(Rsize1);
 
             for (int i = 0; i < 3; i++)
             {
                 Turtle.TurnRight();
-                Turtle.Move(25);
+                Turtle.Move(Rsize2);
             }
 
             Turtle.Angle = 135;
-            Turtle.Move(35);
+            Turtle.Move(Rsize3);
+        }
+        static void Oword(int Osize)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.TurnRight();
+                Turtle.Move(Osize);
+            }
+        }
+        static void Pword(int Psize1, int Psize2)
+        {
+            Turtle.Move(Psize1);
 
-            //Last "T"
+            for (int i = 0; i < 3; i++)
+            {
+                Turtle.TurnRight();
+                Turtle.Move(Psize2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 10;
+
+            //TORT     
+            //"T" letter
+            Turtle.X = 100;
+            Turtle.Y = 200;
+            Turtle.Angle = 0;
+
+            Tword(50,25);
+
+            //"O" letter
+            Turtle.X = 150;
+            Turtle.Y = 150;
+            Turtle.Angle = 0;
+
+            Oword(50);
+
+            //"R" letter
+            Turtle.X = 225;
+            Turtle.Y = 200;
+            Turtle.Angle = 0;
+
+            Rword(50,25,35);
+
+            //"T" letter
             Turtle.X = 300;
             Turtle.Y = 200;
-
             Turtle.Angle = 0;
-            Tletter();
+
+            Tword(50,25);
+
+            //"OR"
+            //"O" letter
+            Turtle.X = 75;
+            Turtle.Y = 270;
+            Turtle.Angle = 0;
+
+            Oword(30);
+
+            //"R" letter
+            Turtle.X = 125;
+            Turtle.Y = 300;
+            Turtle.Angle = 0;
+
+            Rword(30, 15, 25);
+
+            //"Port"
+            //"P"
+            Turtle.X = 200;
+            Turtle.Y = 400;
+            Turtle.Angle = 0;
+
+            Pword(100,50);
+
+            //"O" letter
+            Turtle.X = 275;
+            Turtle.Y = 300;
+            Turtle.Angle = 0;
+
+            Oword(100);
+
+            //"R" letter
+            Turtle.X = 400;
+            Turtle.Y = 400;
+            Turtle.Angle = 0;
+
+            Rword(100, 50, 70);
+
+            //"T" letter
+            Turtle.X = 525;
+            Turtle.Y = 400;
+            Turtle.Angle = 0;
+
+            Tword(100, 50);
         }
+
+
     }
 }
